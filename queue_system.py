4f51e1cea42c73902b2c33a1f4706a4f41758c5e
Penny <pkiourti@gmail.com>
2022-03-20 13:52:04 -0400

    add comments

--- queue_system.py
+++ queue_system.py
@@ -2,11 +2,15 @@ from multiprocessing import Process, Semaphore
 import time
 import argparse
 
+# number of API calls we handle simultaneously
 free_resources = Semaphore(4)
 
 def handle_api_request():
     time.sleep(3)
 
+# stub function for API request
+# check if there are free resources before handling the request
+# release resources after handling the request
 def server_task(request_id):
     free_resources.acquire()
     handle_api_request()
@@ -24,12 +28,14 @@ if __name__ == '__main__':
     requests = args.requests
     tracking_seconds = args.tracking_seconds
 
+    # start a process for each API request
     processes = []
     for request_id in range(requests):
         p = Process(target=server_task, args=(request_id,))
         p.start()
         processes.append(p)
 
+    # print PID, status, exit code of each process until there all not alive
     while True:
         statuses = [p.is_alive() for p in processes]
         pids = [str(p.pid) for p in processes]
